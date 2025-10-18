8de27ad12073e3c2678760d2c95b3afd3c99fc76
andreykarganov <karganov.an@yandex.ru>
2025-10-18 11:25:54 +0300

    style fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,12 @@ import Home from './pages/Home.tsx'
 import { Footer } from './components/layout/Footer.tsx'
 import { Header } from './components/layout/Header.tsx'
 
+import './styles/style.css'
 import './styles/footer.css'
 import './styles/header.css'
-import './styles/mobile.css'
 import './styles/navigation.css'
-import './styles/style.css'
+import './styles/mobile.css'
+
 
 
 function App() {
